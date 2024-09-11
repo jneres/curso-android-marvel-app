4d573d7d8371fc3b76b9a48397ddccd0780a68d6
jneres <TR045783@comgas.com.br>
2024-09-10 23:53:19 -0300

    Cria interfaces para fontes de dados

---
+++ core/src/main/java/com/example/core/data/repository/CharactersRemoteDataSource.kt
@@ -0,0 +1,8 @@
+package com.example.core.data.repository
+
+import java.time.temporal.TemporalQueries
+
+interface CharactersRemoteDataSource<T> {
+
+    suspend fun fetchCharacters(queries: Map<String, String>): T
+}
