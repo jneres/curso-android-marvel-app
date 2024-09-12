cce91c9b26db40479822d752226d49fd0674534e
jneres <TR045783@comgas.com.br>
2024-09-11 22:20:00 -0300

    Implementa repositóri na camada de frameword

--- core/src/main/java/com/example/core/data/repository/CharactersRemoteDataSource.kt
+++ core/src/main/java/com/example/core/data/repository/CharactersRemoteDataSource.kt
@@ -3,6 +3,5 @@ package com.example.core.data.repository
 import java.time.temporal.TemporalQueries
 
 interface CharactersRemoteDataSource<T> {
-
     suspend fun fetchCharacters(queries: Map<String, String>): T
 }
